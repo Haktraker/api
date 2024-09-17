7b5c28fc3302a86186ba5ee59ad69326253562ae
Haktraker <b.badr@haktraknetworks.com>
2024-09-17 03:55:14 +0300

    ReportCyberAttackTrends

--- models/Reports/ReportCyberAttackTrends.js
+++ models/Reports/ReportCyberAttackTrends.js
@@ -16,6 +16,11 @@ const ReportCyberAttackTrendsSchema = new mongoose.Schema({
   year: { type: String }, // Add year field
 });
 
+ReportCyberAttackTrendsSchema.index(
+  { day: 1, month: 1, year: 1 },
+  { unique: true }
+);
+
 module.exports = mongoose.model(
   "ReportCyberAttackTrends",
   ReportCyberAttackTrendsSchema
